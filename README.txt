c9fecfd2db4faa2fe7f769de02c73a95af2ca460
Hiago Faria <faria.hiagoo@gmail.com>
2021-10-04 15:12:39 -0300

    Entrada Classe User, Memo Fyle no Readme e planos dia 2

--- README.txt
+++ README.txt
@@ -5,3 +5,11 @@ Segundo passo: Integrar com banco de dados
 Terceiro passo: Fazer deploy em servidor
 
 1.1: Criar sistema de gerenciamento de usuários (1/N) Meta: finalizar até 5/10
+
+Relato dia 1: Criei objeto USER, que vai receber atributos e as funções de permissão
+
+1.2 Criar a função e as permissões que cada user terá assim como as funções.
+    Permissões Conferente: Lançar quantidade de entrada e saida de produtos do estoque.	   Atributos: Produto, Quantidade, NF de movimentação.
+    Permissões Vendedor: Lançar venda, alterar valor dentro da margem estabelecida pelo    gerente, consultar estoque;
+    Permissões gerente: Gerar relatório de vendas individual, cadastrar usuários, autor    desconto maior que margem para vendedor.
+    Permissões cliente: Verificar estoque e verificar preço.
--- USUARIOS.py
+++ USUARIOS.py
@@ -1,3 +1,31 @@
+#User levels: 0-admin, 1-gerente, 2-conferente, 3-vendedor, 4-cliente(via_API)
+
 class user():
-    def __init__(self,):
-        self
+    def __init__(self, nome,  lvl_user = 1, nickname, password, email, CPF):
+        self.nome = nome 
+        self.lvl_user = lvl_user
+        self.nickname = nickname
+        self.password = password
+        self.email = email
+        self.CPF = CPF
+        print("Construtor finalizado!!!")
+
+    def Cadastro(self, nome,  lvl_user, nickname, password, email, CPF):
+        while(True):
+            err = ["!@#$%*/+.-"]
+            self.nome = input("Nome: ")
+            if isdigit(self.nome) or (err in self.nome):
+                print("Nome inválido. Caracteres especiais
+                e/ou números não permitidos")
+                self.nome = input("Nome: ")
+            else:
+                break
+        self.nickname = input("Digite o login: ")
+        self.password = input("Digite a senha: ")
+        self.email = input("Digite o e-mail: ")
+        self.CPF= input("Digite o CPF: ")
+
+    def NIVEL_ACESSO(self, lvl_user)
+        self.lvl_user = lvl_user
+        permissoes(self.lvl_user)
+        
